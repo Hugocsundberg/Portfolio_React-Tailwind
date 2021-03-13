99ae410c93af3d14d06fef1560250736e9d7b5c1
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-03-14 00:20:33 +0100

    Add text-animation on page-load

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import AboutMe from './Components/AboutMe';
 import FirstProject from './Components/FirstProject';
 import Hero from './Components/Hero';
@@ -8,11 +9,13 @@ import tree from './Components/tree.png'
 import treeWide from './Components/treeWide.png'
 import treeMid from './Components/treeMid.png'
 
+
 function App() {
+
   return (
-    <div className="App bg-gradient-to-b from-blue-100 to-purple-100">
+    <div className="App bg-gradient-to-b from-blue-100 to-purple-100 font-sans">
       <MoveShape />
-      <Hero />
+      <Hero className="animate-me" />
       <FirstProject/>
       <Project id="2" name="SaladBowl" imageLong={tree} imageMid={treeMid} imageWide={treeWide} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam."/>
       <Project id="3" left={true} name="SaladBowl" imageLong={tree} imageMid={treeMid} imageWide={treeWide} body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. A, gravida neque massa bibendum erat laoreet ornare vitae elementum. Diam porta massa in eget sed congue pretium at arcu. Scelerisque etiam tristique pulvinar dui eleifend praesent mi mi. Eu mauris quam vel condimentum egestas habitant malesuada vitae diam."/>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import anime from 'animejs/lib/anime.es.js';
+import { easings, stagger } from 'animejs';
 
 const Hero = () => {
+    const h1El = useRef()
+    const p1El = useRef()
+    const p2El = useRef()
+    const p3El = useRef()
+    // setInterval(() => {
+    //   console.log(heroDiv.current)
+    // }, 1000);
+  
+    useEffect(() => {
+      anime({
+        targets: [p1El.current, h1El.current, p2El.current, p3El.current],
+        translateY: [-250, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(100),
+        easing: 'spring(3, 100, 13, 1)'
+      });
+    }, []);
+
     return (
         <div className="pt-36 bd-blur relative h-screen w-screen bg-white bg-opacity-70 ">
-            <p className="pl-8 md:pl-12 text-3xl mb-8 md:md-12">Hugo Sundberg</p>
-            <h1 className="pl-8 md:pl-12 text-6xl md:text-7xl font-bold mb-36">Portfolio.</h1>
-            <p className="pl-8 md:pl-12 text-3xl font-bold mb-4 md:mb-8">Projects</p>
-            <p className="pl-8 md:pl-12 text-3xl font-bold">About me</p>
+            <p ref={p1El} className="pl-8 md:pl-12 text-3xl mb-8 md:md-12 font-light">Hugo Sundberg</p>
+            <h1 ref={h1El} className="pl-8 md:pl-12 text-6xl md:text-7xl font-bold mb-36">Portfolio.</h1>
+            <p ref={p2El} className="pl-8 md:pl-12 text-3xl font-bold mb-4 md:mb-8">Projects</p>
+            <p ref={p3El} className="pl-8 md:pl-12 text-3xl font-bold">About me</p>
         </div>
     );
 }
